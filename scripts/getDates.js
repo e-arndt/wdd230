b4751833a0280a81a5ef24423e04ea534eb144f1
e-arndt <earndt1701@gmail.com>
2025-03-06 20:39:04 -0800

    footer in progress

---
+++ scripts/getDates.js
@@ -0,0 +1,8 @@
+document.getElementById("lastModified").innerHTML = document.lastModified;
+
+const currentDate = new Date();
+const yearSpan = document.querySelector('#currentYear');
+
+yearSpan.innerText = currentDate.getFullYear();
+ const userWeight = "";
+ const num = "";
