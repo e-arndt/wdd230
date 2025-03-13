046ef32d46000eaff7c6a507f2e2bd6a316e840f
e-arndt <earndt1701@gmail.com>
2025-03-13 01:51:47 -0700

    hamburger menu and resizings

--- scripts/getDates.js
+++ scripts/getDates.js
@@ -4,3 +4,13 @@ const currentDate = new Date();
 const yearSpan = document.querySelector('#currentYear');
 
 yearSpan.innerText = currentDate.getFullYear();
+
+// Store the selected elements that we are going to use. 
+const mainnav = document.querySelector('.navigation')
+const hambutton = document.querySelector('#menu');
+
+// Add a click event listender to the hamburger button and use a callback function that toggles the list element's list of classes.
+hambutton.addEventListener('click', () => {
+	mainnav.classList.toggle('show');
+	hambutton.classList.toggle('show');
+});
