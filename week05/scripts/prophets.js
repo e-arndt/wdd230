cc6596de9882de94536cfe6438156bdede89a53b
e-arndt <earndt1701@gmail.com>
2025-04-02 15:55:11 -0700

    prophets activity

--- week05/scripts/prophets.js
+++ week05/scripts/prophets.js
@@ -1,3 +1,10 @@
+
+function getPresidencyRange(prophet) {
+  let deathYear = prophet.death ? parseInt(prophet.death.split(' ').pop()) : "Present"; 
+  let startYear = deathYear !== "Present" ? deathYear - prophet.length : "Ongoing";
+  return `${startYear} - ${deathYear}`;
+}
+
 const url = 'https://brotherblazzard.github.io/canvas-content/latter-day-prophets.json';
 
 const cards = document.querySelector('#cards');
@@ -18,18 +25,31 @@ const displayProphets = (prophets) => {
         let card = document.createElement('section');
         let fullName = document.createElement('h2'); // fill in the blank
         let portrait = document.createElement('img');
+        let birthDate = document.createElement('p');
+        let birthPlace = document.createElement('p');
+        let presidencyRange = document.createElement('p');
+        
+
     
         // Build the h2 content out to show the prophet's full name
         fullName.textContent = `${prophet.name} ${prophet.lastname}`; // fill in the blank
+        birthDate.textContent = `Date of Birth: ${prophet.birthdate}`;
+        birthPlace.textContent = `Place of Birth: ${prophet.birthplace}`;
+        presidencyRange.textContent = `President of Church: ${getPresidencyRange(prophet)}`;
+
         // Build the image portrait by setting all the relevant attributes
         portrait.setAttribute('src', prophet.imageurl);
         portrait.setAttribute('alt', `Portrait of ${prophet.name} ${prophet.lastname}`); // fill in the blank
         portrait.setAttribute('loading', 'lazy');
         portrait.setAttribute('width', '340');
         portrait.setAttribute('height', '440');
+
     
         // Append the section(card) with the created elements
         card.appendChild(fullName); //fill in the blank
+        card.appendChild(birthDate);
+        card.appendChild(birthPlace);
+        card.appendChild(presidencyRange);
         card.appendChild(portrait);
     
         cards.appendChild(card);
