f9f0a2ee51e61702ce85bb3e9c142d57ab6e89a5
e-arndt <earndt1701@gmail.com>
2025-04-03 00:59:24 -0700

    links json

--- scripts/links.js
+++ scripts/links.js
@@ -1,1 +1,12 @@
 const baseURL = "https://e-arndt.github.io/wdd230/";
+
+const linksURL = "https://e-arndt.github.io/wdd230/data/links.json";
+
+async function getLinks() {
+    const response = await fetch(linksURL);
+    const data = await response.json();
+    console.log(data);
+    // displayLinks(data);
+  }
+  
+  getLinks();
